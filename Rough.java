a387130c27b138d29721bd032162f137174e148e
Siddharth6937 <siddharth6937@gmail.com>
2024-02-25 06:54:48 +0530

    learing js

---
+++ Rough.java
@@ -0,0 +1,11 @@
+public class Rough{
+
+    public static void main(String[] args) {
+        int a = 12;
+        if(a == 12){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
---
+++ project/GalleryProject/index.js
@@ -0,0 +1,9 @@
+let pannel_1 = document.getElementById("pannel-1");
+let pannel_2 = document.getElementById("pannel-2");
+
+let pannel_3 = document.getElementById("pannel-3");
+
+let pannel_4 = document.getElementById("pannel-4");
+
+let pannel_5 = document.getElementById("pannel-5");
+
