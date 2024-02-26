bbc32141fd999fabe5a8872b62a0833a514537a1
Siddharth6937 <siddharth6937@gmail.com>
2024-02-26 09:47:58 +0530

    JS String methods

---
+++ JSString.js
@@ -0,0 +1,63 @@
+let x = "javaScript + HTML + CSS + Bootstrap";
+// let y = "javaScript";
+
+// if(x === y ){
+//     console.log(true);
+// }
+
+
+let z1 = new String("JavaScript");
+// console.log(z1.valueOf() === x);
+
+// let z2 = new String("JavaScript");
+
+// console.log(z1 === z2);
+
+                /* Various Methods in Java Script */
+
+// console.log(x.length);
+// console.log(x.toUpperCase());
+// console.log(x.toLowerCase());
+// console.log(x);
+
+// console.log(x[x.length-1]);
+// console.log(x[1]);
+// console.log(x[0]);
+// console.log(x.indexOf("5"));
+// console.log(x.lastIndexOf("a"));
+
+// console.log(x.continue )
+
+
+// includes() : returns true if substrign present in the string, else return false;
+// console.log(x.includes("Script"));
+// console.log(x.startsWith("javaScript"));
+// console.log(x.startsWith("sid"));
+// console.log(x.endsWith("Script"));
+// console.log(x.endsWith("script"));
+
+// console.log(x[4]);
+// console.log(x.charAt(4));
+// console.log(x.charCodeAt(4));
+
+// console.log(x.replace("javaScript", "Siddharth"));
+// console.log(x.replaceAll("a",""));
+
+/*
+        //  /js/gi   -> g referes to global, i refers to  case insensetive
+
+        // match() -> It returns all the matched ocurrences(***)
+            console.log(x.match(/S/gi));
+        // search() -> It returns the count of all occurences(***)
+            console.log(x.search(/S/gi));
+*/
+
+            // let z = " Siddharth ";
+            // let g = "priyanka ";
+
+            // console.log(x+z+g);
+            // console.log(x.concat(z,g));
+
+console.log(x.slice(0 ));
+//     .substr  , .substring, 
+console.log()
