3a56494baa9a6aa85784faea4952d8dc43e2e169
Siddharth6937 <siddharth6937@gmail.com>
2024-02-26 14:56:23 +0530

    Calculator Project: Digits, . coverd. operators started

--- project/Calculator/index.js
+++ project/Calculator/index.js
@@ -1,5 +1,30 @@
 var displayArea = document.getElementById("display");
 
-function buttonClicked(digitOrOperator){
-    displayArea.innerText = displayArea.innerText + digitOrOperator;
+function isOperator(buttonText) {
+    if (buttonText == '+' || buttonText == '-' || buttonText == '%' || buttonText == '/' || buttonText == '*') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function buttonClicked(buttonText) {
+    if (displayArea.innerText === '0') {
+
+        if (buttonText != '0' && buttonText != '00') {
+            if (buttonText == "." || isOperator(buttonText)) {
+                displayArea.innerText = '0' + buttonText;
+            } else {
+                displayArea.innerText = buttonText;
+            }
+        } else {
+            displayArea.innerText = '0';
+        }
+    } else {
+        if (isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator((buttonText))) {
+            displayArea.innerText = displayArea.innerText.replace(displayArea.innerText[displayArea.innerText.length - 1], buttonText); 
+        } else {
+            displayArea.innerText = displayArea.innerText + buttonText;
+        }
+    }
 }
